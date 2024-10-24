d29262850be672f29f54c043c353da67b871f4be
@mervinart <mjlunford@gmail.com>
2024-10-24 10:35:15 -0400

    Add code1.py

---
+++ code1.py
@@ -0,0 +1,8 @@
+def greet(name, department, program):
+	print("welcome, " + name)
+	print("you are part of " + department)
+	print("language of choice is " + program)
+
+greet("merv", "IT", "python")
+greet("swerv", "web page", "javascript")
+
